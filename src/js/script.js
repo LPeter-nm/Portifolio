bb6af7551e9450b57994e1b277a1567e17e91ce3
LPeter-nm <pg.nasmatos@outlook.com>
2024-07-04 09:29:09 -0300

    feat: add portfolio a GH

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".typing", {
+  strings: ["Desenvolvedor back-end", "Estudante", "Programador da Developers Community"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+});
