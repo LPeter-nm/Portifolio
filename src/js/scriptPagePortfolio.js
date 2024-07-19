02c4e9aa3f4a60f6aa8458e53e2301615f0aa49e
LPeter-nm <pg.nasmatos@outlook.com>
2024-07-19 12:53:15 -0300

    fix: comprimindo js

--- src/js/scriptPagePortfolio.js
+++ src/js/scriptPagePortfolio.js
@@ -1,12 +1,1 @@
-const backPage = document.querySelector(".back");
-backPage.addEventListener("click", () => {
-  window.location = "index.html"
-})
-
-window.addEventListener('load', () => {
-  if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add("fa-sun")
-  } else {
-    dayNight.querySelector("i").classList.add("fa-moon")
-  }
-})
+const backPage = document.querySelector(".back"); backPage.addEventListener("click", () => { window.location = "index.html" }), window.addEventListener("load", () => { document.body.classList.contains("dark") ? dayNight.querySelector("i").classList.add("fa-sun") : dayNight.querySelector("i").classList.add("fa-moon") });
