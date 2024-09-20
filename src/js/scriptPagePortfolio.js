039f24f72bf91f2ecc4738e34639c60834bc315f
LPeter-nm <pg.nasmatos@outlook.com>
2024-09-19 23:01:19 -0300

    feat: adicionando projetos e ajeitando a parte de projetos

--- src/js/scriptPagePortfolio.js
+++
@@ -1,1 +0,0 @@
-const backPage = document.querySelector(".back"); backPage.addEventListener("click", () => { window.location = "index.html" }), window.addEventListener("load", () => { document.body.classList.contains("dark") ? dayNight.querySelector("i").classList.add("fa-sun") : dayNight.querySelector("i").classList.add("fa-moon") });
